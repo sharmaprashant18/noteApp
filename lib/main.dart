d1a1ad7911f8069385c7ae53700e045e82ae386b
prashant <ps3348644@gmail.com>
2024-02-09 16:41:07 +0545

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/UI/homepage.dart';
+
 import 'package:get/get.dart';
+import 'package:todoapp/UI/homepage.dart';
 
 void main() {
   runApp(Home());
@@ -17,3 +18,60 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+
+
+
+// COUNTERAPP
+// class HomePage extends StatefulWidget {
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   int number = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               '$number',
+//               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 TextButton(
+//                     onPressed: () {
+//                       setState(() {
+//                         number++;
+//                       });
+//                     },
+//                     child: Text(
+//                       'Increment',
+//                       style:
+//                           TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+//                     )),
+//                 TextButton(
+//                     onPressed: () {
+//                       setState(() {
+//                         number--;
+//                       });
+//                     },
+//                     child: Text(
+//                       'Decerement',
+//                       style:
+//                           TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+//                     ))
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
