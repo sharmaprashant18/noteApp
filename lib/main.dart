e2f01efd07411e2daa53adf7a6785715f4e90c5a
prashant <ps3348644@gmail.com>
2024-02-11 18:08:10 +0545

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,147 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:get/get.dart';
+
+// PROVIDER
+// void main() {
+//   runApp(ProviderScope(child: Home()));
+// }
+
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Consumer(builder: (context, ref, child) {
+//           // final number = ref.watch(sampleProvider).number;
+//           final number = ref.watch(sampleProvider).number;
+//           return Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 '$number',
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   TextButton(
+//                       onPressed: () {
+//                         ref.read(sampleProvider).addition();
+//                       },
+//                       child: Text('Increase')),
+//                   TextButton(
+//                       onPressed: () {
+//                         ref.read(sampleProvider).Decrease();
+//                       },
+//                       child: Text('Decrease')),
+//                 ],
+//               ),
+//             ],
+//           );
+//         }),
+//       ),
+//     );
+//   }
+// }
+
+// // final sampleProvider = Provider((ref) => CounterProvide());
+
+// // class CounterProvide {
+// //   int number = 0;
+// //   void increment() {
+// //     number++;
+// //   }
+
+// //   void decrement() {
+// //     number--;
+// //   }
+// // }
+
+// CHANGENOTIFIER&CHANGENOTIFIERPROVIDER
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:get/get.dart';
+
+// void main() {
+//   runApp(Home());
+// }
+
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Consumer(builder: (context, ref, child) {
+//           final number = ref.watch(SampleCounterApp).number;
+//           return Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text('$number'),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   TextButton(
+//                       onPressed: () {
+//                         ref.read(SampleCounterApp).increase();
+//                       },
+//                       child: Text('Increase')),
+//                   TextButton(
+//                       onPressed: () {
+//                         ref.read(SampleCounterApp).decrease();
+//                       },
+//                       child: Text('Decrease'))
+//                 ],
+//               )
+//             ],
+//           );
+//         }),
+//       ),
+//     );
+//   }
+// }
+
+// final SampleCounterApp = ChangeNotifierProvider((ref) => CounterApp());
+
+// class CounterApp extends ChangeNotifier {
+//   int number = 0;
+//   void increase() {
+//     number++;
+//     notifyListeners();
+//   }
+
+//   void decrease() {
+//     number--;
+//     notifyListeners();
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:todoapp/UI/homepage.dart';
 
 void main() {
-  runApp(ProviderScope(child: Home()));
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle());
+  runApp(Home());
 }
 
 class Home extends StatelessWidget {
@@ -15,76 +150,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-final sample = Provider((ref) => CounterProvider());
-
-class CounterProvider {
-  int number = 0;
-  void increment() {
-    number++;
-  }
-
-  void decrement() {
-    number--;
-  }
-}
-
-
-// COUNTERAPP
-// class HomePage extends StatefulWidget {
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   int number = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               '$number',
-//               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 TextButton(
-//                     onPressed: () {
-//                       setState(() {
-//                         number++;
-//                       });
-//                     },
-//                     child: Text(
-//                       'Increment',
-//                       style:
-//                           TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-//                     )),
-//                 TextButton(
-//                     onPressed: () {
-//                       setState(() {
-//                         number--;
-//                       });
-//                     },
-//                     child: Text(
-//                       'Decerement',
-//                       style:
-//                           TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-//                     ))
-//               ],
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
