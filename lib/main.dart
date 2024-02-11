7c1ae6d6c7fa5abf4911ea06f2f5dd67a5eb76ea
prashant <ps3348644@gmail.com>
2024-02-11 18:58:56 +0545

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -136,6 +136,66 @@
 //     notifyListeners();
 //   }
 // }
+
+// StateProvider
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:get/get.dart';
+// // import 'package:todoapp/UI/homepage.dart';
+
+// void main() {
+//   runApp(ProviderScope(child: Home()));
+// }
+
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       home: HomePage(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Consumer(builder: (context, ref, child) {
+//           final number = ref.watch(SampleCounterApp);
+//           return Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text('$number'),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   TextButton(
+//                       onPressed: () {
+//                         ref.read(SampleCounterApp.notifier).state++;
+//                       },
+//                       child: Text('Increase')),
+//                   TextButton(
+//                       onPressed: () {
+//                         ref.read(SampleCounterApp.notifier).state--;
+//                       },
+//                       child: Text('Decrease'))
+//                 ],
+//               )
+//             ],
+//           );
+//         }),
+//       ),
+//     );
+//   }
+// }
+
+// final SampleCounterApp = StateProvider<int>((ref) => 0);
+
+// TODOAPP
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todoapp/UI/homepage.dart';
