6d39f103929b6cc9bdeb6b2ca8539e35e77aaff4
sharmaprashant18 <ps3348644@gmail.com>
2024-02-14 20:58:30 +0545

    Changed

--- lib/provider/todoprovider.dart
+++ lib/provider/todoprovider.dart
@@ -4,19 +4,19 @@
 // final todoProvider =
 //     StateNotifierProvider<TodoProvider, List>((ref) => TodoProvider([]));
 
-// // class TodoProvider extends StateNotifier<List<Todo>> {
-// //   TodoProvider(super.state);
-// //   add(Todo todos) {
-// //     state.add(todos);
-// //   }
+// class TodoProvider extends StateNotifier<List<Todo>> {
+//   TodoProvider(super.state);
+//   add(Todo todos) {
+//     state.add(todos);
+//   }
 
-// //   delete(Todo todos) {
-// //     state.remove(todos);
-// //   }
-// // }
+//   delete(Todo todos) {
+//     state.remove(todos);
+//   }
+// }
 
 // // List<Todo> todo = [Todo(title: '', id: DateTime.now().toString())];
-
+// STATENOTIFIERPROVIDER
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todoapp/todo_model/todomodel1.dart';
 
